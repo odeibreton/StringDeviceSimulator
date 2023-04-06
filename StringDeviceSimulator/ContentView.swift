56f584ab1caccb4a3a13193582ad030d8a3e4f1a
Odei Bretón <odeibreton@hotmail.com>
2023-04-06 20:08:06 +0100

    Working version

--- StringDeviceSimulator/ContentView.swift
+++ StringDeviceSimulator/ContentView.swift
@@ -6,14 +6,47 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = MainViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Peripheral manager state:")
+                Text(viewModel.blePeripheralState)
+                    .bold()
+            }
+            
+            HStack {
+                Text("Is peripheral advertising:")
+                Text(String(viewModel.isAdvertising))
+                    .bold()
+            }
+            
+            HStack {
+                Text("Subscribed to readings characteristic:")
+                Text(String(viewModel.subscribedToReadingsCharacteristic))
+                    .bold()
+            }
+            
+            HStack {
+                Text("Is recording:")
+                Text(String(viewModel.isRecording))
+                    .bold()
+            }
+            
+            if viewModel.isAdvertising {
+                Button("Stop advertising") {
+                    viewModel.stopAdvertising()
+                }
+            } else {
+                Button("Start advertising") {
+                    viewModel.startAdvertising()
+                }
+            }
         }
         .padding()
     }
---
+++ StringDeviceSimulator/MainViewModel.swift
@@ -0,0 +1,133 @@
+//
+//  MainViewModel.swift
+//  StringDeviceSimulator
+//
+//  Created by Odei Bretón on 06/04/2023.
+//
+
+import Foundation
+import CoreBluetooth
+
+class MainViewModel : NSObject, ObservableObject {
+    @Published private(set) var blePeripheralState: String = "Unknown"
+    @Published private(set) var isAdvertising: Bool = false
+    @Published private(set) var subscribedToReadingsCharacteristic: Bool = false
+    @Published private(set) var isRecording: Bool = false
+    
+    private let serviceId = CBUUID(string: "2cdaa35b-be1e-40d4-aba0-3add764a6a8b")
+    private let readingsCharacteristicId = CBUUID(string: "85844cc1-2eac-4744-9b9d-462cfd8debd1")
+    private let isRecordingCharacteristicId = CBUUID(string: "69872099-e938-4e1e-99c4-74afc913d553")
+    
+    private let readingsCharacteristic: CBMutableCharacteristic
+    private let isRecordingCharacteristic: CBMutableCharacteristic
+    
+    private var rng = SystemRandomNumberGenerator()
+    private let timer = RepeatingTimer(timeInterval: 0.01)
+    
+    private var peripheralManager: CBPeripheralManager!
+    
+    override init() {
+        readingsCharacteristic = CBMutableCharacteristic(type: readingsCharacteristicId, properties: .notify, value: nil, permissions: .readable)
+        isRecordingCharacteristic = CBMutableCharacteristic(type: isRecordingCharacteristicId, properties: .write, value: nil, permissions: .writeable)
+        
+        super.init()
+        
+        self.peripheralManager = CBPeripheralManager(delegate: self, queue: DispatchQueue.main)
+        
+        timer.eventHandler = sendReading
+    }
+    
+    func startAdvertising() {
+        peripheralManager.startAdvertising(nil)
+    }
+    
+    func stopAdvertising() {
+        peripheralManager.stopAdvertising()
+        isAdvertising = false
+    }
+    
+    private func initialisePeripheralManager() {
+        let service = CBMutableService(type: serviceId, primary: true)
+        service.characteristics = [
+            readingsCharacteristic,
+            isRecordingCharacteristic
+        ]
+        
+        self.peripheralManager.removeAllServices()
+        self.peripheralManager.add(service)
+    }
+    
+    private func sendReading() -> Void {
+        let number = rng.next()
+        
+        let data = withUnsafeBytes(of: number) { Data($0) }
+        
+        peripheralManager.updateValue(data, for: readingsCharacteristic, onSubscribedCentrals: nil)
+    }
+}
+
+extension MainViewModel : CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .unknown:
+            blePeripheralState = "Unknown"
+        case .resetting:
+            blePeripheralState = "Resetting"
+        case .unsupported:
+            blePeripheralState = "Unsupported"
+        case .unauthorized:
+            blePeripheralState = "Unauthorized"
+        case .poweredOff:
+            blePeripheralState = "Powered off"
+        case .poweredOn:
+            blePeripheralState = "Powered on"
+            initialisePeripheralManager()
+        @unknown default:
+            blePeripheralState = "Unknown"
+        }
+        
+        print("New peripheral state: \(blePeripheralState)")
+    }
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        self.isAdvertising = peripheral.isAdvertising
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        if characteristic.uuid == readingsCharacteristicId {
+            subscribedToReadingsCharacteristic = true
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        if characteristic.uuid == readingsCharacteristicId {
+            subscribedToReadingsCharacteristic = false
+            timer.suspend()
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        for request in requests {
+            if request.characteristic.uuid == isRecordingCharacteristicId {
+                guard let value = request.value,
+                      value.count == 1 else {
+                    continue
+                }
+                
+                let byte = value[0]
+                
+                switch byte {
+                case 0:
+                    isRecording = false
+                    timer.suspend()
+                case 1:
+                    isRecording = true
+                    timer.resume()
+                default:
+                    print("Unrecognised value received: \(byte)")
+                }
+            }
+        }
+    }
+}
