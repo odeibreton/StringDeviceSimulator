136b64ff214165f33302ecc96e8428e0eb3fd9e9
Odei Bretón <odeibreton@hotmail.com>
2023-04-06 18:35:13 +0100

    Initial Commit

---
+++ StringDeviceSimulator/StringDeviceSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringDeviceSimulatorApp.swift
+//  StringDeviceSimulator
+//
+//  Created by Odei Bretón on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct StringDeviceSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
